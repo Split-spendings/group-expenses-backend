20e316b82162071939bc53b3fc80be9a49233fbd
Oleksandr Bieliakov <abel9k@gmail.com>
2021-06-06 15:55:07 +0200

    initial commit

---
+++ src/main/java/com/splitspendings/groupexpensesbackend/GroupExpensesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.splitspendings.groupexpensesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupExpensesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupExpensesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitspendings/groupexpensesbackend/GroupExpensesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitspendings.groupexpensesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupExpensesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
