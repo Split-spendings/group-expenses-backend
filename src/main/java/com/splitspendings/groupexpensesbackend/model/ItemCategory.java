dc020d2ce764faba8ffc3f376b6ee525dc1abc93
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-08-02 20:03:45 +0300

    added ItemCategory + repo

---
+++ src/main/java/com/splitspendings/groupexpensesbackend/model/ItemCategory.java
@@ -0,0 +1,34 @@
+package com.splitspendings.groupexpensesbackend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_category")
+
+@Getter
+@Setter
+public class ItemCategory {
+    public static final int TITLE_MIN_LENGTH = 1;
+    public static final int TITLE_MAX_LENGTH = 50;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", foreignKey = @ForeignKey(name = "fk_item_category_group"))
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id", foreignKey = @ForeignKey(name = "fk_item_category_app_user"))
+    private AppUser createdBy;
+
+    @Column(name = "title", nullable = false, length = TITLE_MAX_LENGTH)
+    private String title;
+
+    @Column(name = "visible", nullable = false)
+    private Boolean visible = true;
+}
