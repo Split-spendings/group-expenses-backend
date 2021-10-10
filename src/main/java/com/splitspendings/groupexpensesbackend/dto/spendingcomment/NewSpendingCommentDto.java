498c3b4c9f872ab448496cae22217946cbe1b7d1
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-10-10 16:51:54 +0200

    added required values

--- src/main/java/com/splitspendings/groupexpensesbackend/dto/spendingcomment/NewSpendingCommentDto.java
+++ src/main/java/com/splitspendings/groupexpensesbackend/dto/spendingcomment/NewSpendingCommentDto.java
@@ -1,12 +1,29 @@
 package com.splitspendings.groupexpensesbackend.dto.spendingcomment;
 
+import com.splitspendings.groupexpensesbackend.util.TrimUtil;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 public class NewSpendingCommentDto {
+
+    public NewSpendingCommentDto(Long spendingID, Long addedByAppUserId, String message) {
+        this.spendingID = spendingID;
+        this.addedByAppUserId = addedByAppUserId;
+        setMessage(message);
+    }
+
     @NotNull
-    private Long spendingID;
-    //todo
+    private final Long spendingID;
+
+    @NotNull
+    private final Long addedByAppUserId;
+
+    @NotNull
+    private String message;
+
+    private void setMessage(String message){
+        this.message = TrimUtil.trimAndRemoveExtraSpaces(message);
+    }
 }
