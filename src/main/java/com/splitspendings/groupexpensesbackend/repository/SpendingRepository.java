4c5452078cac6232c5da10a5e1e717e209d5866b
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-10-14 19:05:12 +0200

    deleted unused method

--- src/main/java/com/splitspendings/groupexpensesbackend/repository/SpendingRepository.java
+++ src/main/java/com/splitspendings/groupexpensesbackend/repository/SpendingRepository.java
@@ -3,21 +3,10 @@ package com.splitspendings.groupexpensesbackend.repository;
 import com.splitspendings.groupexpensesbackend.model.Group;
 import com.splitspendings.groupexpensesbackend.model.Spending;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 public interface SpendingRepository extends JpaRepository<Spending, Long> {
 
     List<Spending> findAllByAddedByGroupMembershipGroup(Group group);
-
-    @Query( "SELECT s " +
-            "FROM Spending s " +
-            "LEFT JOIN fetch s.comments " +
-            "WHERE s.id = ?1 " +
-            "AND s.addedByGroupMembership.appUser.id = ?2 " +
-            "AND s.addedByGroupMembership.active = true")
-    Optional<Spending> findByIdFetchComments(Long spendingId, UUID userId);
 }
