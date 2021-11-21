3831c9087e4168b34c27d5ae4a192f7c2777c531
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2021-11-21 16:04:46 +0100

    added Payoff create/update controller

--- src/main/java/com/splitspendings/groupexpensesbackend/dto/payoff/NewPayoffDto.java
+++ src/main/java/com/splitspendings/groupexpensesbackend/dto/payoff/NewPayoffDto.java
@@ -1,5 +1,44 @@
 package com.splitspendings.groupexpensesbackend.dto.payoff;
 
+import com.splitspendings.groupexpensesbackend.model.Payoff;
+import com.splitspendings.groupexpensesbackend.model.enums.Currency;
+import com.splitspendings.groupexpensesbackend.util.TrimUtil;
+import lombok.Data;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
 public class NewPayoffDto {
-    //todo
+
+    @NotNull
+    @Size(min = Payoff.TITLE_MIN_LENGTH, max = Payoff.TITLE_MAX_LENGTH)
+    private String title;
+
+    /**
+     * amount of a Payoff to be saved, cannot be negative or zero
+     */
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
+    private BigDecimal amount;
+
+    private byte[] receiptPhoto;
+
+    @NotNull
+    private Currency currency;
+
+    private long groupId;
+
+    @NotNull
+    private UUID paidForAppUser;
+
+    @NotNull
+    private UUID paidToAppUser;
+
+    public void setTitle(String title) {
+        this.title = TrimUtil.trimAndRemoveExtraSpaces(title);
+    }
 }
--- src/main/java/com/splitspendings/groupexpensesbackend/service/impl/PayoffServiceImpl.java
+++ src/main/java/com/splitspendings/groupexpensesbackend/service/impl/PayoffServiceImpl.java
@@ -4,20 +4,26 @@ import com.splitspendings.groupexpensesbackend.dto.payoff.NewPayoffDto;
 import com.splitspendings.groupexpensesbackend.dto.payoff.PayoffDto;
 import com.splitspendings.groupexpensesbackend.dto.payoff.UpdatePayoffDto;
 import com.splitspendings.groupexpensesbackend.mapper.PayoffMapper;
+import com.splitspendings.groupexpensesbackend.model.AppUser;
+import com.splitspendings.groupexpensesbackend.model.Group;
 import com.splitspendings.groupexpensesbackend.model.Payoff;
 import com.splitspendings.groupexpensesbackend.repository.PayoffRepository;
+import com.splitspendings.groupexpensesbackend.service.AppUserService;
 import com.splitspendings.groupexpensesbackend.service.GroupMembershipService;
+import com.splitspendings.groupexpensesbackend.service.GroupService;
+import com.splitspendings.groupexpensesbackend.service.IdentityService;
 import com.splitspendings.groupexpensesbackend.service.PayoffService;
 import com.splitspendings.groupexpensesbackend.util.LogUtil;
+import com.splitspendings.groupexpensesbackend.util.ValidatorUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Validator;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -29,7 +35,10 @@ public class PayoffServiceImpl implements PayoffService {
 
     private final PayoffRepository payoffRepository;
 
+    private final AppUserService appUserService;
+    private final IdentityService identityService;
     private final GroupMembershipService groupMembershipService;
+    private final GroupService groupService;
 
     private final PayoffMapper payoffMapper;
 
@@ -69,11 +78,41 @@ public class PayoffServiceImpl implements PayoffService {
 
     @Override
     public PayoffDto createPayoff(NewPayoffDto newPayoffDto) {
-        throw new NotImplementedException();
+        ValidatorUtil.validate(validator, newPayoffDto);
+        Long groupId = newPayoffDto.getGroupId();
+        Group group = groupService.groupModelById(groupId);
+
+        verifyUsersActiveMembership(groupId, newPayoffDto.getPaidForAppUser(), newPayoffDto.getPaidToAppUser());
+        AppUser currentAppUser = appUserService.appUserModelById(identityService.currentUserID());
+        AppUser paidForAppUser = appUserService.appUserModelById(newPayoffDto.getPaidForAppUser());
+        AppUser paidToAppUser = appUserService.appUserModelById(newPayoffDto.getPaidToAppUser());
+
+        Payoff payoff = payoffMapper.newPayoffDtoToPayoff(newPayoffDto, group, currentAppUser, paidForAppUser, paidToAppUser);
+        payoffRepository.save(payoff);
+
+        return payoffMapper.payoffToPayoffDto(payoff);
     }
 
     @Override
     public PayoffDto updatePayoff(Long id, UpdatePayoffDto updatePayoffDto) {
-        throw new NotImplementedException();
+        ValidatorUtil.validate(validator, updatePayoffDto);
+        Payoff payoff = payoffModelById(id);
+        Group group = payoff.getGroup();
+
+        verifyUsersActiveMembership(group.getId(), updatePayoffDto.getPaidForAppUser(), updatePayoffDto.getPaidToAppUser());
+        AppUser currentAppUser = appUserService.appUserModelById(identityService.currentUserID());
+        AppUser paidForAppUser = appUserService.appUserModelById(updatePayoffDto.getPaidForAppUser());
+        AppUser paidToAppUser = appUserService.appUserModelById(updatePayoffDto.getPaidToAppUser());
+
+        payoffMapper.copyUpdatePayoffDtoToPayoff(updatePayoffDto, currentAppUser, paidForAppUser, paidToAppUser, payoff);
+        payoffRepository.save(payoff);
+
+        return payoffMapper.payoffToPayoffDto(payoff);
+    }
+
+    private void verifyUsersActiveMembership(Long groupId, UUID paidForAppUser, UUID paidToAppUser){
+        groupMembershipService.verifyCurrentUserActiveMembershipByGroupId(groupId);
+        groupMembershipService.verifyUserActiveMembershipByGroupId(paidForAppUser, groupId);
+        groupMembershipService.verifyUserActiveMembershipByGroupId(paidToAppUser, groupId);
     }
 }
